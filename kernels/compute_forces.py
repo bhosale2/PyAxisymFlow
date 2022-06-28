f3a82c1037d9268724242b34d5799ce503319668
Gauravu2 <89496329+Gauravu2@users.noreply.github.com>
2022-06-28 13:22:43 -0500

    New kernels

---
+++ kernels/compute_forces.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def compute_forces(R, part_char_func, rho_f, brink_lam, u_z, U_z_cm_part, part_vol, dt, diff):
+    """
+    computes penalization and unsteady forces
+    """
+    F_pen = rho_f * brink_lam * np.sum(R * part_char_func * (u_z - U_z_cm_part))
+    F_un =  (diff*part_vol) /dt
+
+    return F_pen,F_un 
