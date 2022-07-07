1748e6989d11e269b9d22e7823433ec422cf2339
Gauravu2 <89496329+Gauravu2@users.noreply.github.com>
2022-07-07 13:36:08 -0500

    Update kernels/theory_rigid_slab.py
    Co-authored-by: Yashraj Bhosale <yashraj.iitb@gmail.com>

--- kernels/theory_rigid_slab.py
+++ kernels/theory_rigid_slab.py
@@ -9,8 +9,8 @@ def theory_axisymmetric_rigid_slab_spatial(omega,nu,R_tube):
 	func_soln = A*besselj(0, lam1 * y)
 	soln_lam = lambdify(y, func_soln)
 	y_range=  np.linspace(0,R_tube,100)
-	Soln = soln_lam(y_range)
-	return Soln
+	spatial_soln = soln_lam(y_range)
+	return spatial_soln
 
 def theory_axisymmetric_rigid_slab_temporal(U_0, spatial_soln, omega,t):
 	temp_soln = U_0 *np.real(2*spatial_soln*np.exp(-1j*omega*(t))) 
