64ee6da253fef8cc1ec6f69c217761d1004d8fdc
Bhosale <bhosale2@illinois.edu>
2022-07-30 12:34:36 -0500

    refac example for package style

--- examples/PeriodicFlowPastSphere/periodic_flow_past_sphere.py
+++ examples/PeriodicFlowPastSphere/periodic_flow_past_sphere.py
@@ -1,28 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 import os
-
-sys.path.append("../../")
-from set_sim_params import grid_size_z, grid_size_r, dx, eps, LCFL, domain_AR
-from utils.plotset import plotset
-from utils.custom_cmap import lab_cmp
-from utils.dump_vtk import vtk_init, vtk_write
-from kernels.brinkmann_penalize import brinkmann_penalize
-from kernels.compute_forces import compute_force_on_body
-from kernels.smooth_Heaviside import smooth_Heaviside
-from kernels.kill_boundary_vorticity_sine import (
+from pyaxisymflow.utils.plotset import plotset
+from pyaxisymflow.utils.custom_cmap import lab_cmp
+from pyaxisymflow.kernels.brinkmann_penalize import brinkmann_penalize
+from pyaxisymflow.kernels.smooth_Heaviside import smooth_Heaviside
+from pyaxisymflow.kernels.kill_boundary_vorticity_sine import (
     kill_boundary_vorticity_sine_r,
-    kill_boundary_vorticity_sine_z,
 )
-from kernels.FastDiagonalisationStokesSolver import FastDiagonalisationStokesSolver
-from kernels.periodic_boundary_ghost_comm import gen_periodic_boundary_ghost_comm
-from kernels.compute_velocity_from_psi import compute_velocity_from_psi_unb_periodic
-from kernels.compute_vorticity_from_velocity import (
-    compute_vorticity_from_velocity_unb_periodic,
+from pyaxisymflow.kernels.FastDiagonalisationStokesSolver import (
+    FastDiagonalisationStokesSolver,
 )
-from kernels.advect_particle import advect_vorticity_via_particles_periodic
-from kernels.diffusion_RK2 import diffusion_RK2_periodic
+from pyaxisymflow.kernels.periodic_boundary_ghost_comm import (
+    gen_periodic_boundary_ghost_comm,
+)
+from pyaxisymflow.kernels.compute_velocity_from_psi import (
+    compute_velocity_from_psi_periodic,
+)
+from pyaxisymflow.kernels.compute_vorticity_from_velocity import (
+    compute_vorticity_from_velocity_periodic,
+)
+from pyaxisymflow.kernels.diffusion_RK2 import diffusion_RK2_periodic
+from pyaxisymflow.kernels.advect_vorticity_via_eno3 import gen_advect_vorticity_via_eno3
+
+
+# global settings
+grid_size_z = 256
+domain_AR = 0.5
+dx = 1.0 / grid_size_z
+grid_size_r = int(domain_AR * grid_size_z)
+CFL = 0.1
+eps = np.finfo(float).eps
+num_threads = 4
 
 plotset()
 plt.figure(figsize=(5 / domain_AR, 5))
@@ -51,23 +60,10 @@ vorticity = 0 * Z
 penal_vorticity = 0 * Z
 temp_vorticity = 0 * Z
 psi = 0 * Z
-avg_psi = 0 * Z
-avg_vort = 0 * Z
-avg_part_char_func = 0 * Z
 u_z = 0 * Z
 u_r = 0 * Z
-u_z_old = 0 * Z
-u_r_old = 0 * Z
 u_z_upen = 0 * Z
 u_r_upen = 0 * Z
-inside_bubble = 0 * Z
-u_z_breath = 0 * Z
-u_r_breath = 0 * Z
-Z_double, R_double = np.meshgrid(z, z)
-z_particles = Z_double.copy()
-r_particles = R_double.copy()
-vort_double = 0 * Z_double
-vort_particles = 0 * vort_double
 fotoTimer = 0.0
 it = 0
 freqTimer = 0.0
@@ -77,13 +73,10 @@ Z_cm = 0.85
 R_cm = 0.0
 t = 0.0
 T = []
-diff = 0
-F_total = 0
 #  create char function
 phi0 = -np.sqrt((Z - Z_cm) ** 2 + (R - R_cm) ** 2) + r_cyl
 char_func = 0 * Z
 smooth_Heaviside(char_func, phi0, moll_zone)
-part_mass = np.sum(char_func * R)
 
 # solving poisson only on the inner grid
 psi_inner = psi[..., ghost_size:-ghost_size].copy()
@@ -93,13 +86,14 @@ FD_stokes_solver = FastDiagonalisationStokesSolver(
     dx,
     bc_type="homogenous_neumann_along_r_and_periodic_along_z",
 )
-vtk_image_data, temp_vtk_array, writer = vtk_init()
+advect_vorticity_via_eno3 = gen_advect_vorticity_via_eno3(
+    dx, grid_size_r, grid_size_z, num_threads=num_threads
+)
 
 # solver loop
 while t < tEnd:
 
     # kill vorticity at boundaries
-    # kill_boundary_vorticity_sine_z(vorticity, Z, 3, dx)
     kill_boundary_vorticity_sine_r(vorticity, R, 3, dx)
 
     # solve for stream function and get velocity
@@ -109,7 +103,7 @@ while t < tEnd:
     )
     psi[..., ghost_size:-ghost_size] = psi_inner
 
-    compute_velocity_from_psi_unb_periodic(u_z, u_r, psi, R, dx, per_communicator)
+    compute_velocity_from_psi_periodic(u_z, u_r, psi, R, dx, per_communicator)
 
     # add free stream
     prefac_x = 1.0
@@ -121,8 +115,6 @@ while t < tEnd:
     u_r[...] += U_0 * prefac_y
 
     if fotoTimer >= fotoTimer_limit or t == 0:
-        fotoTimer = 0.0
-
         fotoTimer = 0.0
         levels = np.linspace(-0.1, 0.1, 25)
         plt.contourf(
@@ -164,32 +156,19 @@ while t < tEnd:
     # get dt
     dt = min(
         0.9 * dx**2 / 4 / nu,
-        LCFL / (np.amax(np.fabs(vorticity)) + eps),
-        0.01 * freqTimer_limit,
+        CFL / (np.amax(np.fabs(u_z) + np.fabs(u_r)) + eps),
     )
 
     # penalise velocity (particle)
     u_z_upen[...] = u_z.copy()
     u_r_upen[...] = u_r.copy()
     brinkmann_penalize(brink_lam, dt, char_func, 0.0, 0.0, u_z_upen, u_r_upen, u_z, u_r)
-    compute_vorticity_from_velocity_unb_periodic(
+    compute_vorticity_from_velocity_periodic(
         penal_vorticity, u_z - u_z_upen, u_r - u_r_upen, dx, per_communicator
     )
     vorticity[...] += penal_vorticity
 
-    advect_vorticity_via_particles_periodic(
-        z_particles,
-        r_particles,
-        vort_particles,
-        vorticity,
-        Z_double,
-        R_double,
-        grid_size_r,
-        u_z,
-        u_r,
-        dx,
-        dt,
-    )
+    advect_vorticity_via_eno3(vorticity, u_z, u_r, dt)
 
     # diffuse vorticity
     diffusion_RK2_periodic(vorticity, temp_vorticity, R, nu, dt, dx, per_communicator)
@@ -205,5 +184,6 @@ while t < tEnd:
 
 os.system("rm -f 2D_advect.mp4")
 os.system(
-    "ffmpeg -r 8 -s 3840x2160 -f image2 -pattern_type glob -i 'snap_*.png' -vcodec libx264 -crf 15 -pix_fmt yuv420p -vf 'crop=trunc(iw/2)*2:trunc(ih/2)*2' 2D_advect.mp4"
+    "ffmpeg -r 8 -s 3840x2160 -f image2 -pattern_type glob -i 'snap_*.png' -vcodec"
+    " libx264 -crf 15 -pix_fmt yuv420p -vf 'crop=trunc(iw/2)*2:trunc(ih/2)*2' 2D_advect.mp4"
 )
