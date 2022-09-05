2517718e6415b7fe4f1cfdc34df4a0c72c034450
Fan Kiat Chan <fchan5@illinois.edu>
2022-09-04 21:09:26 -0500

    feat: run cases for validation with Kotas 2007

---
+++ examples/SpheroidStreaming/spheroid_streaming_validate_kotas.py
@@ -0,0 +1,44 @@
+import os
+from spheroid_streaming import simulate_oscillating_spheroid
+
+# (spheroid_AR, M_sq) pairs to validate from Kotas 2007
+cases = [
+    (0.75, 28.195),
+    (0.75, 42.4298),
+    (1.0, 31.3415),
+    (1.0, 41.5948),
+    (1.0, 53.5910),
+    (1.0, 59.7499),
+    (1.0, 66.8879),
+    (1.0, 80.1726),
+    (1.0, 93.8293),
+    (1.0, 100.2640),
+    (1.0, 149.4063),
+    (1.3, 37.1179),
+    (1.3, 42.7611),
+    (1.3, 74.2809),
+    (1.3, 73.6915),
+    (1.3, 110.5705),
+    (1.3, 111.0121),
+    (2.0, 48.7771),
+]
+
+if __name__ == "__main__":
+
+    for (spheroid_AR, M_sq) in cases:
+        dirname = f"spheroidAR{spheroid_AR}_M_sq{M_sq}"
+        if os.path.exists(dirname):
+            os.system(f"rm -rf {dirname}")
+        os.makedirs(dirname, exist_ok=True)
+
+        os.chdir(dirname)
+        print(f"Simulating AR={spheroid_AR}, M_sq={M_sq}")
+        simulate_oscillating_spheroid(
+            M_sq=M_sq,
+            radius=0.075,
+            spheroid_AR=spheroid_AR,
+            grid_size_z=512,
+            plot_figure=True,
+            save_vtk=True,
+        )
+        os.chdir("../")
