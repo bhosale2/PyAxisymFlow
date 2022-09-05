5c905f6617bbb1d5118c19d31e5040bfaf0d845a
Fan Kiat Chan <fchan5@illinois.edu>
2022-09-05 11:52:58 -0500

    move validation cases to csv

--- examples/SpheroidStreaming/spheroid_streaming_validate_kotas.py
+++ examples/SpheroidStreaming/spheroid_streaming_validate_kotas.py
@@ -1,30 +1,10 @@
 import os
+import numpy as np
 from spheroid_streaming import simulate_oscillating_spheroid
-
-# (spheroid_AR, womersley_square) pairs to validate from Kotas 2007
-cases = [
-    (0.75, 28.195),
-    (0.75, 42.4298),
-    (1.0, 31.3415),
-    (1.0, 41.5948),
-    (1.0, 53.5910),
-    (1.0, 59.7499),
-    (1.0, 66.8879),
-    (1.0, 80.1726),
-    (1.0, 93.8293),
-    (1.0, 100.2640),
-    (1.0, 149.4063),
-    (1.3, 37.1179),
-    (1.3, 42.7611),
-    (1.3, 74.2809),
-    (1.3, 73.6915),
-    (1.3, 110.5705),
-    (1.3, 111.0121),
-    (2.0, 48.7771),
-]
 
 if __name__ == "__main__":
 
+    cases = np.loadtxt("experimental_data_Kotas_2007.csv", delimiter=",", skiprows=1)
     for (spheroid_AR, womersley_square) in cases:
         dirname = f"spheroidAR{spheroid_AR}_womersley_square{womersley_square}"
         if os.path.exists(dirname):
