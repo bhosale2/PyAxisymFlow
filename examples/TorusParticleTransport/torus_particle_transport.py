318fbc1c9e3e9983d1cccd8de640197d56773eaf
Songyuan Cui <cui20@illinois.edu>
2022-09-14 16:11:43 -0500

    Fix: added dx in CFL time step updates

--- examples/TorusParticleTransport/torus_particle_transport.py
+++ examples/TorusParticleTransport/torus_particle_transport.py
@@ -238,7 +238,7 @@ while t < tEnd:
     # get dt
     dt = min(
         0.9 * dx**2 / 4 / nu,
-        CFL / (np.amax(np.fabs(u_z) + np.fabs(u_r)) + eps),
+        CFL * dx / (np.amax(np.fabs(u_z) + np.fabs(u_r)) + eps),
         LCFL / (np.amax(np.fabs(vorticity)) + eps),
         0.01 * freqTimer_limit,
     )
